fbfc18d520a18fe2e9aa60d5affe907dba114115
진우민 <woomir@gmail.com>
2019-03-11 23:55:22 +0900

    Coating 조건 테이블의 최초 숨김 컬럼 설정 변경

--- database/conditionOfCoating.php
+++ database/conditionOfCoating.php
@@ -511,7 +511,7 @@ $(document).ready(function(){
           postfixButtons: [ 'colvisRestore' ]
         }],
         columnDefs: [
-            {targets: [5,8,9,14,17,20,21,23,-2],
+            {targets: [5,8,9,14,17,18,19,20,21,22,23,24,25,26,27,29,30,31,32,33,34,35,36,37,38,39,40,-2],
              visible: false}
         ]
    });
