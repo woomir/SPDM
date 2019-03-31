4c91748bca869f45e62253cb868e7577fbdc535e
진우민 <woomir@gmail.com>
2019-04-01 01:26:52 +0900

    test

--- lib/menu.php
+++ lib/menu.php
@@ -100,7 +100,7 @@ li.action{
   </div>
   </nav> -->
 
-  <nav id="menuNavbar" class="navbar navbar-expand-lg navbar-dark bg-dark" style="clear:both; position: fixed;">
+  <nav id="menuNavbar" class="navbar navbar-expand-lg navbar-dark bg-dark" >
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
     </button>
