8b3baa99509ccdb9783033efe65c2de64457b108
진우민 <woomir@gmail.com>
2019-06-07 10:15:29 +0900

    file upload system update

--- dataManage/analysisPwlab/file_manage.php
+++ dataManage/analysisPwlab/file_manage.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 include '../db.php';
-// var_dump($_POST);
-// var_dump($_FILES);
 
  if(!empty($_FILES))
  {
@@ -18,8 +16,10 @@ $uploadOk = 1;
 $total = count($_FILES['files']['name']);
 
 for ($i=0; $i < $total; $i++){
-  $file_name = basename($_FILES["files"]["name"][$i]);
+  $file_name - iconv('UTF-8', 'cp949', $_FILES["files"]["name"][$i]);
+  $file_name = str_replace(array("#","%"),"", $file_name);
   $file_raw_name = $_FILES["files"]["name"][$i];
+  $file_raw_name = str_replace(array("#","%"), "", $file_raw_name);
   $file_type = $_FILES["files"]["type"][$i];
   $file_size = $_FILES["files"]["size"][$i];
   $target_file = $target_dir . $file_name;
@@ -28,12 +28,12 @@ for ($i=0; $i < $total; $i++){
 
     // Check if file already exists
   if (file_exists($target_file) && ($target_file != $target_dir)) {
-    echo "Sorry, ".$file_name." already exists.<br>";
+    echo "Sorry, ".$file_raw_name." already exists.<br>";
     $uploadOk = 0;
   } else{
     // Check file size (30MB이상)
     if ($_FILES["files"]["size"][$i] > 30000000) {
-      echo "Sorry, ".$file_name." is too large.<br>";
+      echo "Sorry, ".$file_raw_name." is too large.<br>";
       $uploadOk = 0;
     } else {
       //샘플명과 타입, 파일이름과 동일한 데이터가 있는지 확인
@@ -47,7 +47,7 @@ for ($i=0; $i < $total; $i++){
             sampleNo, powderType, fileName, fileType, fileSize) VALUES (
               '".$sampleNo."', '".$powderType."', '".$file_raw_name."', '".$file_type."', ".$file_size.")";
 
-          echo 'File Uploaded ('.$file_name.')<br>';
+          echo 'File Uploaded ('.$file_raw_name.')<br>';
 
           mysqli_query($connect, $query);
         }
@@ -65,13 +65,16 @@ for ($i=0; $i < $total; $i++){
   }
 }
  } else{
-  $fileName = $_POST["filename"];
+  $fileName = iconv('UTF-8', 'cp949', $_POST["filename"]);
+  $fileName = str_replace(array("#","%"), "", $fileName);
+  $sqlFileName = $_POST["filename"];
+  $sqlFileName = str_replace(array("#","%"),"",$sqlFileName);
   $filePath = "./uploads/".$fileName;
-  $sql = "DELETE FROM filepwlabtbl WHERE fileName = '".$fileName."'";
+  $sql = "DELETE FROM filepwlabtbl WHERE fileName = '".$sqlFileName."'";
     if(mysqli_query($connect, $sql))
     {
       unlink($filePath);
-      echo 'File Deleted ('.$fileName.')<br>';
+      echo 'File Deleted ('.$sqlFileName.')<br>';
     } else {
       echo 'error';
     }
